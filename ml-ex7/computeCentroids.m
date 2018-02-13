e37d20f15e2fc8aab31a699d2be80efeb645606d
IsotopeZ <gcptien@gmail.com>
2018-02-12 22:15:52 -0800

    Part 1 complete

--- ml-ex7/computeCentroids.m
+++ ml-ex7/computeCentroids.m
@@ -25,6 +25,10 @@ centroids = zeros(K, n);
 %
 % Note: You can use a for-loop over the centroids to compute this.
 %
+for k = 1:K
+	index = idx == k;
+	centroids(k,:) = mean(X(index,:));
+end
 
 
 
--- ml-ex7/findClosestCentroids.m
+++ ml-ex7/findClosestCentroids.m
@@ -20,7 +20,21 @@ idx = zeros(size(X,1), 1);
 %
 % Note: You can use a for-loop over the examples to compute this.
 %
+m = size(X,1);
 
+for i = 1:m
+	close_centroid = 0;
+	bestDist = 1000;
+	for j = 1:K
+		d =  X(i,:) - centroids(j,:);
+		d_square = d * d';
+		if (d_square < bestDist)
+			bestDist = d_square;
+			close_centroid = j;
+		end
+	end
+	idx(i) = close_centroid;
+end
 
 
 
